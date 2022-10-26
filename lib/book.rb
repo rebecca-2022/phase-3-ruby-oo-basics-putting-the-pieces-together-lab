035328df8723f255c01b3ace7984e7a4c37efc8a
rebecca njeri <rebecca.njeri@student.moringaschool.com>
2022-10-26 20:58:52 +0300

    complete

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,16 @@
+class Book 
 
+    attr_accessor :title, :author, :page_count, :genre
+    #initialize method
+    def initialize (title)
+        @title = title
+    end
+    #turn_page method
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+    
+
+end
 
